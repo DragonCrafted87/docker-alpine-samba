a6546437ece7e3d01734a4594323619e7f03dbd5
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2020-07-01 21:43:55 -0500

    samba doesn't like symlinks apperently work around that

--- root/scripts/setup_samba.py
+++ root/scripts/setup_samba.py
@@ -9,12 +9,13 @@ from python_logger import create_logger #pylint: disable=import-error
 def main():
   logger = create_logger(Path(__file__).stem)
 
-  if not Path('/mnt/config/var_lib_samba').exists():
+  if Path('/mnt/config/var_lib_samba').exists():
+    logger.info(f'Linking Samba Config to Mounted Config.')
+    system('rm -rf /var/lib/samba')
+    system('cp -rf /mnt/config/var_lib_samba /var/lib/samba ')
+  else:
     logger.info(f'Copying Samba Config to Mounted Config.')
     system('cp -rf /var/lib/samba /mnt/config/var_lib_samba')
-  logger.info(f'Linking Samba Config to Mounted Config.')
-  system('rm -rf /var/lib/samba')
-  system('ln -s /mnt/config/var_lib_samba /var/lib/samba ')
 
 if __name__ == "__main__":
   main()
